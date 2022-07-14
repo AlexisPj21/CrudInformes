e2a9b033b83cecec6abd4e1c2ec733597ce0c8f7
Alexis Pantoja <ajr.pantoja@yavirac.edu.ec>
2022-07-13 22:40:34 -0500

    Nuevo inicio

---
+++ informe-backend/src/main/java/yavirac/edu/informebackend/InformeBackendApplication.java
@@ -0,0 +1,13 @@
+package yavirac.edu.informebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformeBackendApplication.class, args);
+	}
+
+}
