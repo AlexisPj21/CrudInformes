ae76e2c2ed1fbb9c5922bf1928b3291b1873317a
Darling Ortiz <dtv.ortiz@yavirac.edu.ec>
2022-07-14 03:10:06 -0500

    front changes

--- informe-fronted/src/app/app-routing.module.ts
+++ informe-fronted/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OpportunityComponent } from './feature/opportunity/opportunity.component';
+import { ProspectComponent } from './feature/prospect/prospect.component';
 
 const routes: Routes = [
   {path: 'opportunity' , component:OpportunityComponent},
-  {path: 'opportunity/id', component:OpportunityComponent}
+  {path: 'opportunity/id', component:OpportunityComponent},
+  {path: 'prospect', component:ProspectComponent},
+  {path: 'prospect/id', component:ProspectComponent}
 ];
 
 @NgModule({
---
+++ informe-fronted/src/app/feature/prospect/prospect.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Prospect } from './prospect';
+import { ProspectService } from './prospect.service';
+
+
+
+@Component({
+  selector: 'app-prospect',
+  templateUrl: './prospect.component.html'
+})
+export class ProspectComponent implements OnInit {
+
+  currentProspect:Prospect ={
+    prospectId:0,
+    name:"",
+    dni:"",
+    source:"",
+    phone:"",
+    address:"",
+    mail:"",
+    created: new Date(),
+    updated: new Date(),
+    enable: false
+  }
+
+  constructor(
+    private prospectService: ProspectService,
+    private activatedRoute: ActivatedRoute
+  )
+
+  { }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(
+      (params) => {
+        let prospectId:string ="";
+        if(params.get("prospect_id")){
+          prospectId = params.get("prospect_id")!;
+          this.findById(parseInt(prospectId));
+        }
+      }
+    )
+  }
+  save ():void{
+    this.prospectService.save(this.currentProspect)
+    .subscribe((response) => {console.log("registro guardado");
+    this.currentProspect={
+      prospectId:0,
+      name: "",
+      dni:"",
+      source:"",
+      phone:"",
+      address:"",
+      mail:"",
+      created: new Date(),
+      updated: new Date(),
+      enable: false
+    }
+  })
+  }
+  findById(prospectId: number): void{
+    this.prospectService.findById(prospectId)
+    .subscribe(
+      (response:Prospect)=>{
+        this.currentProspect = response;
+      }
+    )
+  }
+
+}
