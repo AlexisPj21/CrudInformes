83ee13ea451852c41330f18b831d0145c63f9044
Darling Ortiz <dtv.ortiz@yavirac.edu.ec>
2022-07-14 22:47:21 -0500

    change

--- informe-fronted/src/app/app-routing.module.ts
+++ informe-fronted/src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { OpportunityComponent } from './feature/opportunity/opportunity.component';
+
 
 const routes: Routes = [
-  {path: 'opportunity' , component:OpportunityComponent},
-  {path: 'opportunity/:id', component:OpportunityComponent}
+
 ];
 
 @NgModule({
